503b09fa6fe2884fd9a8e5cdbb28e1dd43acd0ce
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-01-04 22:28:14 +0300

    Добавлена функция создания тасков

--- Server/api/views.py
+++ Server/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerialzer
+from rest_framework.generics import CreateAPIView
+from .serializers import UserSerialzer, TaskSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Task
 
 
 # Create your views here.
@@ -10,3 +12,11 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerialzer
     permission_classes = [AllowAny]
+
+class CreateTaskView(CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
