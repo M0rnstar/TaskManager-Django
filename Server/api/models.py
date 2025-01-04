49421d28b04347f820e9846a8766e96bbfb678f1
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-01-04 22:27:43 +0300

    Добавлена модель тасков

--- Server/api/models.py
+++ Server/api/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+class Task(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(null=True)
+    deadline = models.DateTimeField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
-
-    def __str__(self):
-        return self.title
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
