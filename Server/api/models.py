52a200e8a0a7855090f5f968711edd0063e19bd0
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-01-11 21:35:16 +0300

    Поле Description заменён на Content

--- Server/api/models.py
+++ Server/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=100, null=False)
-    description = models.TextField(null=True)
+    content = models.TextField(null=True)
     deadline = models.DateTimeField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
