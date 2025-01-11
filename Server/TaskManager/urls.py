8d12d47137399d776d51c01bf6170635f81d4477
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-01-11 21:37:43 +0300

    Добавлены маршруты для CRUD запросов карточек

--- Server/TaskManager/urls.py
+++ Server/TaskManager/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView, CreateTaskView
+from api.views import CreateUserView, CreateTaskView, ListTasksView, DeleteTaskView, UpdateTaskView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -9,5 +9,8 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/tasks/create", CreateTaskView.as_view(), name="create_task")
+    path("api/tasks/create", CreateTaskView.as_view(), name="create_task"),
+    path("api/tasks/", ListTasksView.as_view(), name="list_tasks"),
+    path("api/tasks/<int:pk>/delete", DeleteTaskView.as_view(), name="delete_task"),
+    path("api/tasks/<int:pk>/update", UpdateTaskView.as_view(), name="update_task"),
 ]
