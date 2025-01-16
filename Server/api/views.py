7effd3f69dbf40641435124adbbd62a8a7d7368c
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-01-16 21:38:35 +0300

    Обработчик запроса по получению пользователя

--- Server/api/views.py
+++ Server/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
 from .serializers import UserSerialzer, TaskSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Task
@@ -43,3 +44,10 @@ class UpdateTaskView(UpdateAPIView):
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+    
+class UserProfileView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerialzer
+    
+    def get_object(self):
+        return self.request.user
